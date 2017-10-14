38da42fdde52226ae7d5a3074335eaa999a88012
rfulop <rfulop@student.42.fr>
2017-10-14 03:40:54 +0200

    ex02working

--- j09/todo.js
+++ j09/todo.js
@@ -1,60 +1,44 @@
-// function setCookie(cname, cvalue, exdays)
-// {
-//     var date = new Date();
-//     date.setTime(date.getTime() + (exdays * 24 * 60 * 60 * 1000));
-//     var expires = "expires="+date.toUTCString();
-//     var test = cname + "=" + cvalue + ";" + expires + ";path=/";
-//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// }
+window.onload = function ()
+{
+  var lst = document.getElementById("ft_list");                 // Create a <li> node
+  var cookies = decodeURIComponent(getCookie("list"));
+
+  var tab = JSON.parse(cookies);
+  if (cookies)
+  {
+    for (var i = 0; tab[i]; ++i)
+      add(tab[i]);
+  }
+};
 
 function setCookie(sName, sValue)
 {
+  console.log(sValue);
   var today = new Date(), expires = new Date();
   expires.setTime(today.getTime() + (365*24*60*60*1000));
   document.cookie = sName + "=" + encodeURIComponent(sValue) + ";expires=" + expires.toGMTString();
 }
 
-
-window.onload = function ()
+function getCookie(sName)
 {
-
-  // console.log("euh");
-  var lst = document.getElementById("ft_list");                 // Create a <li> node
-  var cookies = decodeURIComponent(getCookie("list"));
-  if (cookies)
-  {
-    console.log("enter");
-    cookies.split('*');
-    console.log(cookies);
-    // for (var i = 0; cookies[i]; ++i)
-    // {
-    //   if (cookies[i] == '\n')
-    //     ;
-    // }
-  //  lst.innerHTML = cookies;
-//    var nodes = document.createTextNode(lst);         // Create a text node
-//    root.appendChild(nodes)
-//    document.body.innerHTML = lst;
-    // console.log(document.body.innerHTML);
-  }
-  else {
-   console.log("non");
-  }
-};
-
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  // console.log(ca.length);
-  for(var i=0;i < ca.length;i++)
-  {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
+  var oRegex = new RegExp("(?:; )?" + sName + "=([^;]*);?");
+  if (oRegex.test(document.cookie))
+    return decodeURIComponent(RegExp["$1"]);
+  else
+    return NULL;
 }
 
+// function getCookie(name) {
+//   var nameEQ = name + "=";
+//   var ca = document.cookie.split(';');
+//   for(var i=0;i < ca.length;i++)
+//   {
+//     var c = ca[i];
+//     while (c.charAt(0)==' ') c = c.substring(1,c.length);
+//     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+//   }
+//   return null;
+// }
 
 function newTodo()
 {
@@ -72,11 +56,12 @@ function add(newTodo)
   div.setAttribute('id', len);
   div.addEventListener('click', remove);
   lst.insertBefore(div, lst.firstChild);
-  var save = lst.textContent + "*";
-  console.log("save = " + save);
-  setCookie("list", save);
-  //  var test = readCookie("list");
-//    console.log(test);
+
+  var childs = lst.children;
+  var cookies = [];
+  for (var i = 0; childs[i]; ++i)
+    cookies.unshift(childs[i].innerHTML);
+ setCookie("list", JSON.stringify(cookies));
 }
 
 function remove()
@@ -87,7 +72,11 @@ function remove()
     var lst = document.getElementById('ft_list');
     var div = document.getElementById(id);
     lst.removeChild(div);
-    var save = lst.textContent;
-    setCookie("list", save);
+
+    var childs = lst.children;
+    var cookies = [];
+    for (var i = 0; childs[i]; ++i)
+      cookies.unshift(childs[i].innerHTML);
+   setCookie("list", JSON.stringify(cookies));
   }
 }
